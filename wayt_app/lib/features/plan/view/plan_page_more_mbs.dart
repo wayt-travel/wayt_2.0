196009d059e7e8e8cea5c922de53dc9116cb7fbc
Marco Trivisonno <marco.trivi2@gmail.com>
2025-03-20 01:08:35 +0100

    feat: delete a plan
    - fix: travel documents fetch
    - ref: use Set instead of List to store user plans map
    
    Closes #30

--- wayt_app/lib/features/plan/view/plan_page_more_mbs.dart
+++ wayt_app/lib/features/plan/view/plan_page_more_mbs.dart
@@ -1,17 +1,57 @@
 import 'package:flext/flext.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 
 import '../../../repositories/repositories.dart';
 import '../../../widgets/widgets.dart';
-import '../../travel_document/bloc/reorder_items/reorder_items_cubit.dart';
+import '../../features.dart';
 
 /// Modal bottom sheet for the plan page shown when the user taps on the more
 /// button in the app bar.
 sealed class PlanPageMoreMbs {
+  static Future<bool> _delete(
+    BuildContext context, {
+    required String id,
+  }) async {
+    return WConfirmDialog.show(
+      context: context,
+      // FIXME: l10n
+      title: 'Are you sure?',
+      onConfirm: () async {
+        final cubit = DeletePlanCubit(
+          travelDocumentRepository: GetIt.I.get(),
+        );
+
+        await context.navRoot
+            .pushBlocListenerBarrier<DeletePlanCubit, DeletePlanState>(
+          bloc: cubit,
+          trigger: () => cubit.onDelete(id),
+          listenWhen: (previous, current) =>
+              current.status.isSuccess || current.status.isFailure,
+          listener: (context, state) {
+            context.navRoot.pop();
+            if (state.status.isSuccess) {
+              context.navRoot.pop();
+            }
+            if (state.status.isFailure) {
+              SnackBarHelper.I.showError(
+                context: context,
+                message: state.error!.userIntlMessage(context),
+              );
+            }
+          },
+        );
+
+        cubit.close().ignore();
+      },
+    );
+  }
+
   /// Shows the modal bottom sheet.
   static void show(BuildContext context) {
     final isReordering = context.read<ReorderItemsCubit>().isReordering;
+    final planId = context.read<TravelDocumentCubit>().travelDocumentId.id;
     ModalBottomSheet.of(context).showActions<void>(
       actions: [
         ModalBottomSheetAction(
@@ -30,7 +70,9 @@ sealed class PlanPageMoreMbs {
               : null,
         ),
         ModalBottomSheetActions.divider,
-        ModalBottomSheetActions.delete(context),
+        ModalBottomSheetActions.delete(context).copyWith(
+          onTap: (_) => _delete(context, id: planId),
+        ),
       ],
     );
   }
