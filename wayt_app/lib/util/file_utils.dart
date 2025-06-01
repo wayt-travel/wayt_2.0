9f8ca2512b140ff2af1eae06c257a4213b8559c7
Marco Trivisonno <marco.trivi2@gmail.com>
2025-06-02 00:27:15 +0200

    feat: add file utils class

---
+++ wayt_app/lib/util/file_utils.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:path/path.dart' as path;
+import 'package:the_umpteenth_logger/the_umpteenth_logger.dart';
+
+/// An abstract class for handling file operations.
+abstract class FileUtils {
+  static const _logger = NthLogger('FileUtils');
+
+  /// Moves a [file] to the [destinationPath].
+  ///
+  /// If the destination path does not exist, it will be created.
+  /// It creates only the structure of the destination path, not the file if is
+  /// is specified in the [destinationPath].
+  ///
+  /// It returns the moved file.
+  static Future<File> moveFile({
+    required File file,
+    required String destinationPath,
+  }) async {
+    final destinationDirectory = Directory(destinationPath);
+    _logger.d('Moving file: ${file.path} to ${destinationDirectory.path}');
+    if (!destinationDirectory.existsSync()) {
+      _logger.d(
+        'Destination directory does not exist, creating: '
+        '${destinationDirectory.path}',
+      );
+      await destinationDirectory.create(recursive: true);
+    }
+    final destinationFilePath = path.join(
+      destinationDirectory.path,
+      path.basename(file.path),
+    );
+
+    // Moves the file to the new destination.
+    await file.rename(destinationFilePath);
+    _logger.d('File moved to: $destinationFilePath');
+    return File(destinationFilePath);
+  }
+}
